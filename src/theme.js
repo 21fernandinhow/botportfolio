8fdbd1302a677787803c391c8a20b2c7f4c7287a
Fernando <21fernandinhow@gmail.com>
2023-04-12 18:23:42 -0300

    change favicon and theme primary-color

--- src/theme.js
+++ src/theme.js
@@ -3,18 +3,18 @@ const body = document.querySelector("body");
 
 function changeTheme(){
     if (body.dataset.theme === "dark") {
-        root.style.setProperty("--bg-color", "#EDEEF7")
-        root.style.setProperty("--bg-radical-color", "#ffffff")
+        root.style.setProperty("--bg-color", "#edeef7");
+        root.style.setProperty("--bg-radical-color", "#ffffff");
         root.style.setProperty("--contrast-color", "#121212");
-        root.style.setProperty("--primary-color", "#7868E6");
-        root.style.setProperty("--secondary-color", "#B8B5FF");
+        root.style.setProperty("--primary-color", "#7868e6");
+        root.style.setProperty("--secondary-color", "#b8b5ff");
         body.dataset.theme = "light"
       } else {
-        root.style.setProperty("--bg-color", "#18122B")
-        root.style.setProperty("--bg-radical-color", "#000015")
-        root.style.setProperty("--contrast-color", "#f5f5f5")
-        root.style.setProperty("--primary-color", "#443C68");
-        root.style.setProperty("--secondary-color", "#393053");
+        root.style.setProperty("--bg-color", "#18122b"); // alternative color #2c2f33
+        root.style.setProperty("--bg-radical-color", "#000015"); // alternative color #23272a
+        root.style.setProperty("--contrast-color", "#f5f5f5"); 
+        root.style.setProperty("--primary-color", "#635985"); // alternative color #1c1c1c
+        root.style.setProperty("--secondary-color", "#393053"); // alternative-color #7868e6
         body.dataset.theme = "dark"
       }
 }
